e2ac1a806f89f1329d69a0c7b1c3ae37cea17ed4
Andreas Niedermair <andreas@niedermair.name>
2016-12-01 21:05:10 +0100

    added ILocator-implementation

---
+++ src/Caliburn.Micro.Contrib.Controller.Extras/Locator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using JetBrains.Annotations;
+
+namespace Caliburn.Micro.Contrib.Controller
+{
+  public class Locator<T> : ILocator<T>
+  {
+    /// <exception cref="ArgumentNullException"><paramref name="locateFn" /> is <see langword="null" /></exception>
+    public Locator([NotNull] Func<T> locateFn)
+    {
+      if (locateFn == null)
+      {
+        throw new ArgumentNullException(nameof(locateFn));
+      }
+      this.LocateFn = locateFn;
+    }
+
+    [NotNull]
+    private Func<T> LocateFn { get; }
+
+    public T Locate()
+    {
+      var instance = this.LocateFn.Invoke();
+
+      return instance;
+    }
+  }
+}
