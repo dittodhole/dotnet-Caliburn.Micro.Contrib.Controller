d471b2dfe6f99b4f065f65b2b18c1d9d6fffe727
Andreas Niedermair <andreas@niedermair.name>
2016-12-05 12:50:09 +0100

    extracted DisposeAction

--- src/Caliburn.Micro.Contrib.Controller.Extras/ControllerRoutine/BlockingRoutine.cs
+++ src/Caliburn.Micro.Contrib.Controller.Extras/ControllerRoutine/BlockingRoutine.cs
@@ -69,27 +69,5 @@ namespace Caliburn.Micro.Contrib.Controller.Extras.ControllerRoutine
     {
       public bool IsBlocked { get; set; }
     }
-
-    public class DisposeAction : IDisposable
-    {
-      /// <exception cref="ArgumentNullException"><paramref name="action" /> is <see langword="null" /></exception>
-      public DisposeAction([NotNull] Action action)
-      {
-        if (action == null)
-        {
-          throw new ArgumentNullException(nameof(action));
-        }
-        this.Action = action;
-      }
-
-      [CanBeNull]
-      private Action Action { get; set; }
-
-      public void Dispose()
-      {
-        this.Action?.Invoke();
-        this.Action = null;
-      }
-    }
   }
 }
---
+++ src/Caliburn.Micro.Contrib.Controller.Extras/DisposeAction.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using JetBrains.Annotations;
+
+namespace Caliburn.Micro.Contrib.Controller.Extras
+{
+  public class DisposeAction : IDisposable
+  {
+    /// <exception cref="ArgumentNullException"><paramref name="action" /> is <see langword="null" /></exception>
+    public DisposeAction([NotNull] Action action)
+    {
+      if (action == null)
+      {
+        throw new ArgumentNullException(nameof(action));
+      }
+      this.Action = action;
+    }
+
+    [CanBeNull]
+    private Action Action { get; set; }
+
+    public void Dispose()
+    {
+      this.Action?.Invoke();
+      this.Action = null;
+    }
+  }
+}
