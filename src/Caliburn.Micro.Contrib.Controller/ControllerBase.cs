bcea2592cf0cf0541ae70ff387ebbf7617cb5428
Andreas Niedermair <andreas@niedermair.name>
2017-01-18 20:33:47 +0100

    simplified inheritance to get rid of CreateScreen-workaround
    SOLID nazi

--- src/Caliburn.Micro.Contrib.Controller/ControllerBase.cs
+++ src/Caliburn.Micro.Contrib.Controller/ControllerBase.cs
@@ -10,90 +10,35 @@ using JetBrains.Annotations;
 namespace Caliburn.Micro.Contrib.Controller
 {
   [PublicAPI]
-  public abstract class ControllerBase : IController
-  {
-    /// <exception cref="ArgumentNullException"><paramref name="routines" /> is <see langword="null" /></exception>
-    protected ControllerBase([NotNull] [ItemNotNull] params IRoutine[] routines)
-    {
-      if (routines == null)
-      {
-        throw new ArgumentNullException(nameof(routines));
-      }
-
-      foreach (var controllerRoutine in routines)
-      {
-        this.RegisterRoutine(controllerRoutine);
-      }
-    }
-
-    public virtual ICollection<IRoutine> Routines { get; } = new List<IRoutine>();
-
-    public abstract IScreen CreateScreen(object options = null);
-
-    /// <exception cref="ArgumentNullException"><paramref name="routine" /> is <see langword="null" /></exception>
-    [NotNull]
-    public virtual T RegisterRoutine<T>([NotNull] T routine) where T : IRoutine
-    {
-      if (routine == null)
-      {
-        throw new ArgumentNullException(nameof(routine));
-      }
-
-      this.Routines.Add(routine);
-
-      return routine;
-    }
-
-    /// <exception cref="ArgumentNullException"><paramref name="routine" /> is <see langword="null" /></exception>
-    public virtual bool UnregisterRoutine<T>([NotNull] T routine) where T : IRoutine
-    {
-      if (routine == null)
-      {
-        throw new ArgumentNullException(nameof(routine));
-      }
-
-      return this.Routines.Remove(routine);
-    }
-
-    /// <exception cref="ArgumentNullException"><paramref name="routines" /> is <see langword="null" /></exception>
-    public virtual void RegisterRoutines([NotNull] [ItemNotNull] IEnumerable<IRoutine> routines)
-    {
-      if (routines == null)
-      {
-        throw new ArgumentNullException(nameof(routines));
-      }
-
-      foreach (var routine in routines)
-      {
-        this.RegisterRoutine(routine);
-      }
-    }
-
-    public virtual Task<object> GetResultAsync(CancellationToken cancellationToken)
-    {
-      return TaskEx.FromResult<object>(null);
-    }
-  }
-
-  [PublicAPI]
-  public abstract class ControllerBase<TScreen> : ControllerBase
+  public abstract class ControllerBase<TScreen> : IController
     where TScreen : IScreen
   {
     /// <exception cref="ArgumentNullException"><paramref name="screenFactory" /> is <see langword="null" /></exception>
     /// <exception cref="ArgumentNullException"><paramref name="routines" /> is <see langword="null" /></exception>
     protected ControllerBase([NotNull] IScreenFactory screenFactory,
-                             [NotNull] [ItemNotNull] params IRoutine[] routines)
-      : base(routines)
+                             [NotNull] [ItemNotNull] ICollection<IRoutine> routines)
     {
       if (screenFactory == null)
       {
         throw new ArgumentNullException(nameof(screenFactory));
       }
+      if (routines == null)
+      {
+        throw new ArgumentNullException(nameof(routines));
+      }
       this.ScreenFactory = screenFactory;
+      this.Routines = routines;
     }
 
     [NotNull]
     private IScreenFactory ScreenFactory { get; }
+
+    public virtual ICollection<IRoutine> Routines { get; }
+
+    public virtual Task<object> GetResultAsync(CancellationToken cancellationToken)
+    {
+      return TaskEx.FromResult<object>(null);
+    }
 
     /// <exception cref="ArgumentNullException"><paramref name="screen" /> is <see langword="null" /></exception>
     /// <exception cref="ArgumentNullException"><paramref name="view" /> is <see langword="null" /></exception>
@@ -266,15 +211,8 @@ namespace Caliburn.Micro.Contrib.Controller
       }
     }
 
-    public override IScreen CreateScreen(object options = null)
-    {
-      var screen = this.CreateScreenImpl(options);
-
-      return screen;
-    }
-
-    [NotNull]
-    public virtual TScreen CreateScreenImpl([CanBeNull] object options = null)
+    /// <exception cref="Exception" />
+    public virtual IScreen CreateScreen(object options = null)
     {
       var screenType = this.GetScreenType(options);
       var mixinProviders = new object[]
