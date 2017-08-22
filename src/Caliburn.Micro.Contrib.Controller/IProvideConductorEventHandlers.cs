35918b427ba9e602526e30eac69934d6951657a3
Andreas Niedermair <andreas@niedermair.name>
2017-08-22 11:19:32 +0200

    added exception documentation

--- src/Caliburn.Micro.Contrib.Controller/IProvideConductorEventHandlers.cs
+++ src/Caliburn.Micro.Contrib.Controller/IProvideConductorEventHandlers.cs
@@ -9,11 +9,13 @@ namespace Caliburn.Micro.Contrib.Controller
   {
     /// <exception cref="ArgumentNullException"><paramref name="screen" /> is <see langword="null" /></exception>
     /// <exception cref="ArgumentNullException"><paramref name="item" /> is <see langword="null" /></exception>
+    /// <exception cref="Exception" />
     void OnActivateItem([NotNull] TScreen screen,
                         [NotNull] TItem item);
 
     /// <exception cref="ArgumentNullException"><paramref name="screen" /> is <see langword="null" /></exception>
     /// <exception cref="ArgumentNullException"><paramref name="item" /> is <see langword="null" /></exception>
+    /// <exception cref="Exception" />
     void OnDeactivateItem([NotNull] TScreen screen,
                           [NotNull] TItem item,
                           bool close);
