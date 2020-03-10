68f622191301a1c14662841982798f9295d0e765
Andreas Niedermair <dev@andreas.niedermair.name>
2020-03-10 09:36:59 +0100

    fixed namespace

--- src/Caliburn.Micro.Contrib.Controller.DynamicProxy/Intercept.cs
+++ src/Caliburn.Micro.Contrib.Controller.DynamicProxy/Intercept.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Threading.Tasks;
 
-namespace Caliburn.Micro.Contrib.Controller
+namespace Caliburn.Micro.Contrib.Controller.DynamicProxy
 {
   [System.Diagnostics.CodeAnalysis.SuppressMessage("Design", "MA0048:File name must match type name", Justification = "<Pending>")]
   public interface IMixinProvider { }
